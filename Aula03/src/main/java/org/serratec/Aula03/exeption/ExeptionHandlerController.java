da4a526619212481184b647023ac77d1dc8db381
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-05-13 19:06:44 -0300

    Adicionando correções feitas na aula

--- Aula03/src/main/java/org/serratec/Aula03/exeption/ExeptionHandlerController.java
+++ Aula03/src/main/java/org/serratec/Aula03/exeption/ExeptionHandlerController.java
@@ -2,12 +2,14 @@ package org.serratec.Aula03.exeption;
 
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -23,28 +25,35 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
           HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        
+          
+            List<String> erros = new ArrayList<>();
+            for(FieldError erro: ex.getBindingResult().getFieldErrors()){
+                erros.add(erro.getField() + ": " + erro.getDefaultMessage());
+            }
         // Criar um objeto de resposta de erro personalizado (ErroResposta) contendo uma mensagem de erro, o status HTTP, 
         // a data e hora do erro e uma lista de mensagens de validação dos campos inválidos
             ErroResposta erroResposta = new ErroResposta("Existem campos inválidos", status.value(), 
-            LocalDateTime.now(), ex.getBindingResult().getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList());
+            LocalDateTime.now(), erros);
             
       // Retornar a resposta de erro personalizada com o status HTTP apropriado
       return super.handleExceptionInternal(ex, erroResposta, headers, status, request);
   }
    
+    
+  
+    
+  
     //   Adicionar um método para lidar com a exceção personalizada RecursoNaoEncontradoException,
    //   que é lançada quando um recurso (como um cliente ou pedido) não é encontrado
-  @ExceptionHandler(RecursoNaoEncontradoException.class)
-    
-     // Criar um método que recebe a exceção e o contexto da requisição, 
+    @ExceptionHandler(RecursoNaoEncontradoException.class)
+    // Criar um método que recebe a exceção e o contexto da requisição, 
     // e retorna uma resposta personalizada de erro (ErroResposta) com uma mensagem de erro, o status HTTP 404 Not Found, a data e hora do erro e a mensagem da exceção
-    public ResponseEntity<Object> handleRecursoNaoEncontradoException(RecursoNaoEncontradoException ex, WebRequest request) {
+    public ResponseEntity<ErroResposta> handleRecursoNaoEncontradoException(RecursoNaoEncontradoException ex, WebRequest request) {
         
         // Criar um objeto de resposta de erro personalizado (ErroResposta) contendo uma mensagem de erro, o status HTTP 404 Not Found,
         // a data e hora do erro e a mensagem da exceção
-        ErroResposta erroResposta = new ErroResposta("Não existe um cliente com esse ID", HttpStatus.NOT_FOUND.value(), 
-            LocalDateTime.now(), List.of(ex.getMessage()));
+        ErroResposta erroResposta = new ErroResposta( ex.getMessage(), HttpStatus.NOT_FOUND.value(), 
+            LocalDateTime.now(), new ArrayList<>());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erroResposta);
     }
   }
